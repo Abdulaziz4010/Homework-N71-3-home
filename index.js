a1c0fe509ca94f5f087c642045bd507c2240ded4
Abdulaziz4010 <abdulazizs707ja@gamil.com>
2024-09-10 17:20:05 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// 1 - masala
+
+// let k = 10;
+
+// let n = 4;
+
+// for (let i = 0; i < n; i++) {
+//   console.log(k);
+// }
+
+// 2 - masala
+
+// let a = 10;
+
+// let b = 2;
+
+// for (let i = 2; i < a; i++) {
+//   console.log(i);
+// }
+
+// console.log(a);
+// console.log(b);
+
+// 3 - masala
+
+// for (let i = 10; i > 2; i--) {
+//   console.log(i);
+// }
+
+// 4 - masala
+
+// let birKgNarxi = 15;
+// let qanchaKg = 2.5;
+
+// let umumiyNarx = birKgNarxi * qanchaKg;
+
+// console.log("Umumiy narx: " + umumiyNarx + " so'm");
